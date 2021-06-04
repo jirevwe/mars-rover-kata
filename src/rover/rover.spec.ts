aca89415fc86b9dd4ef943c183f77e4d11385d34
Raymond Tukpe <rtukpe@gmail.com>
2021-06-04 12:37:55 +0100

    feat: add test to validate that a rover is initialised correctly

---
+++ src/rover/rover.spec.ts
@@ -0,0 +1,16 @@
+describe('rover test sequence', () => {
+  it.each`
+    config
+    ${{ x: 0, y: 0, direction: Direction.NORTH }}
+    ${{ x: 0, y: 1, direction: Direction.SOUTH }}
+    ${{ x: 1, y: 0, direction: Direction.EAST }}
+    ${{ x: 1, y: 1, direction: Direction.WEST }}
+  `('the rover should be initialised correctly', ({ config }) => {
+    const rover = new Rover(config);
+
+    expect(rover).toBeTruthy();
+    expect(rover.position.x).toBe(config.x);
+    expect(rover.position.y).toBe(config.y);
+    expect(rover.direction).toBe(config.direction);
+  });
+});
