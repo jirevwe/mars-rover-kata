ed663ca67199710f48bcf4d70298f5f2e0f1c354
Raymond Tukpe <rtukpe@gmail.com>
2021-06-04 16:57:06 +0100

    feat: add implementation to rotate a rover left by 90 degrees

--- src/rover/rover.ts
+++ src/rover/rover.ts
@@ -1,13 +1,19 @@
 import { RoverConfig, Vector2 } from './typings';
+import { Direction } from './utils';
 
 export class Rover {
   position: Vector2;
   direction: number;
+  private possibleDirections: number;
 
   constructor(readonly config: RoverConfig) {
     this.direction = config.direction;
     this.position = { x: config.x, y: config.y };
+    this.possibleDirections = Object.keys(Direction).length;
   }
 
-  turnLeft() {}
+  turnLeft() {
+    this.direction =
+      (this.direction + this.possibleDirections - 1) % this.possibleDirections;
+  }
 }
--- src/rover/utils.ts
+++ src/rover/utils.ts
@@ -4,3 +4,16 @@ export const Direction = Object.freeze({
   EAST: 1,
   WEST: 3
 });
+
+export function directionMapping(direction: number) {
+  switch (direction) {
+    case Direction.NORTH:
+      return { x: 0, y: 1 };
+    case Direction.SOUTH:
+      return { x: 0, y: -1 };
+    case Direction.EAST:
+      return { x: 1, y: 0 };
+    case Direction.WEST:
+      return { x: -1, y: 0 };
+  }
+}
