772d9c9389841e9c5f8bf0e52cfcbca0ba59fc4b
Raymond Tukpe <rtukpe@gmail.com>
2021-06-04 17:20:43 +0100

    feat: add test case to validate that a rover can rotate right by 90 degrees

--- src/rover/rover.spec.ts
+++ src/rover/rover.spec.ts
@@ -34,4 +34,22 @@ describe('rover test sequence', () => {
     expect(rover.position.y).toBe(y);
     expect(rover.direction).toBe(direction);
   });
+
+  test.each`
+    config                                        | expected
+    ${{ x: 0, y: 0, direction: Direction.NORTH }} | ${{ x: 0, y: 0, direction: Direction.EAST }}
+    ${{ x: 0, y: 0, direction: Direction.WEST }}  | ${{ x: 0, y: 0, direction: Direction.NORTH }}
+    ${{ x: 0, y: 0, direction: Direction.EAST }}  | ${{ x: 0, y: 0, direction: Direction.SOUTH }}
+    ${{ x: 0, y: 0, direction: Direction.SOUTH }} | ${{ x: 0, y: 0, direction: Direction.WEST }}
+  `('should turn a rover 90 degrees to the right', ({ config, expected }) => {
+    const rover = new Rover(config);
+
+    rover.turnRight();
+
+    const { x, y, direction } = expected;
+
+    expect(rover.position.x).toBe(x);
+    expect(rover.position.y).toBe(y);
+    expect(rover.direction).toBe(direction);
+  });
 });
